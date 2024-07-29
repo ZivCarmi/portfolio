00596f1f33220c50021908847170fcc01ebb5285
ZivCarmi <carmi.ziv@gmail.com>
2024-07-29 23:03:04 +0300

    updated projects urls

--- content/projects.ts
+++ content/projects.ts
@@ -6,7 +6,7 @@ export const projects: ProjectType[] = [
     description:
       "Classic Monopoly game with standard rules set that can be played with your friends online.",
     tags: ["React", "Node.js", "Tailwind"],
-    website: "https://monopoly-front.onrender.com",
+    website: "https://monopoly-io.zivi.dev",
     github: "https://github.com/ZivCarmi/monopoly-front",
   },
   {
@@ -14,7 +14,7 @@ export const projects: ProjectType[] = [
     description:
       "Job search platform enables effortless job search, application, and streamlined posting for employers.",
     tags: ["Next.js", "React", "Node.js", "Tailwind"],
-    website: "https://jobsearch-next.vercel.app",
+    website: "https://jobsearch.zivi.dev",
     github: "https://github.com/ZivCarmi/jobsearch-next",
   },
   {
@@ -22,7 +22,7 @@ export const projects: ProjectType[] = [
     description:
       "The famous Candy Crush game - match at least three candies with the same colors to climb up the leaderboard.",
     tags: ["React", "Node.js", "Tailwind"],
-    website: "https://react-candy-crush.vercel.app",
+    website: "https://candy-crush.zivi.dev",
     github: "https://github.com/ZivCarmi/react-candy-crush",
   },
   {
