2016707c72dde8a98cb291d2136c2ceea5570068
ZivCarmi <carmi.ziv@gmail.com>
2025-09-14 20:00:09 +0300

    added Tisot terminal 1 project

--- content/projects.ts
+++ content/projects.ts
@@ -2,6 +2,14 @@ import { ProjectType } from "@/types/Project";
 
 export const projects: ProjectType[] = [
   {
+    title: "Tisot Terminal 1",
+    description:
+      "Interactive real-time flight board for Terminal 1, supporting internationalization and showing updated departures and arrivals with continuously refreshed live data.",
+    tags: ["Next.js", "React", "Node.js", "Tailwind"],
+    website: "https://tisot-terminal1-next.onrender.com",
+    github: "https://github.com/ZivCarmi/tisot-terminal1-next",
+  },
+  {
     title: "Monopolyz",
     description:
       "Classic Monopoly game with standard rules set that can be played with your friends online.",
